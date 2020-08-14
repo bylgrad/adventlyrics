860163c147ee03cd09b252282388039cee0b79cf
bylgrad <bylgrad@gmail.com>
2020-08-14 20:59:13 +0800

    release v1.0

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAQ3GrI7iOU7rja1Y-CT4nIBd_cQiEO0O4",
+    authDomain: "musicplaylist-3bb38.firebaseapp.com",
+    databaseURL: "https://musicplaylist-3bb38.firebaseio.com",
+    projectId: "musicplaylist-3bb38",
+    storageBucket: "musicplaylist-3bb38.appspot.com",
+    messagingSenderId: "612606989778",
+    appId: "1:612606989778:web:5a62be93f3aa907795e40b",
+    measurementId: "G-Q64VXSDT7R"
+  }
 };
